998b806dd0ffb13c3a7cb6afc5c3b1450fe36cc1
Alfonso Cuitiño <alfonso_cuiti@hotmail.com>
2017-01-03 23:08:12 -0300

    agrego archivos para desktop

---
+++ desktop/src/chat/client/AWTChatGui.java
@@ -0,0 +1,87 @@
+package chat.client;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import chat.client.agent.ChatClientAgent;
+
+public class AWTChatGui extends Frame implements ChatGui {
+	private ChatClientAgent myAgent;
+	private TextField writeTf;
+	private TextArea allTa;
+	private ParticipantsFrame participantsFrame;
+	
+	public AWTChatGui(ChatClientAgent a) {
+		myAgent = a;
+		
+		setTitle("Chat: "+myAgent.getLocalName());
+		setSize(getProperSize(256, 320));
+		Panel p = new Panel();
+		p.setLayout(new BorderLayout());
+		writeTf = new TextField();
+		p.add(writeTf, BorderLayout.CENTER);
+		Button b = new Button("Send");
+		b.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+		  	String s = writeTf.getText();
+		  	if (s != null && !s.equals("")) {
+			  	myAgent.handleSpoken(s);
+			  	writeTf.setText("");
+		  	}
+			} 
+		} );
+		p.add(b, BorderLayout.EAST);
+		add(p, BorderLayout.NORTH);
+		
+		allTa = new TextArea();
+		allTa.setEditable(false);
+		allTa.setBackground(Color.white);
+		add(allTa, BorderLayout.CENTER);
+		
+		b = new Button("Participants");
+		b.setBackground(Color.CYAN);
+		b.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (!participantsFrame.isVisible()) {
+					participantsFrame.setVisible(true);
+				}	
+			} 
+		} );
+		add(b, BorderLayout.SOUTH);
+		
+		participantsFrame = new ParticipantsFrame(this, myAgent.getLocalName());
+		
+		addWindowListener(new	WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				myAgent.doDelete();
+			}
+		} );
+		
+		show();
+	}
+	
+	public void notifyParticipantsChanged(String[] names) {
+		if (participantsFrame != null) {
+			participantsFrame.refresh(names);
+		}
+	}
+	
+	public void notifySpoken(String speaker, String sentence) {
+		allTa.append(speaker+": "+sentence+"\n");
+	}
+	
+	Dimension getProperSize(int maxX, int maxY) {
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int x = (screenSize.width < maxX ? screenSize.width : maxX);
+		int y = (screenSize.height < maxY ? screenSize.height : maxY);
+		return new Dimension(x, y);
+	}
+	
+	public void dispose() {
+		participantsFrame.dispose();
+		super.dispose();
+	}
+}
+
+
+
