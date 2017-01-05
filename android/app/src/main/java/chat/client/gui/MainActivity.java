dfc1a7ccd0d73daf09c7760ae0df180e5cd09908
Alfonso Cuitiño <alfonso_cuiti@hotmail.com>
2017-01-04 21:29:23 -0300

    minor refactors

--- android/app/src/main/java/chat/client/gui/MainActivity.java
+++ android/app/src/main/java/chat/client/gui/MainActivity.java
@@ -1,26 +1,3 @@
-/*****************************************************************
-JADE - Java Agent DEvelopment Framework is a framework to develop 
-multi-agent systems in compliance with the FIPA specifications.
-Copyright (C) 2000 CSELT S.p.A. 
-
-GNU Lesser General Public License
-
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation, 
-version 2.1 of the License. 
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the
-Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-Boston, MA  02111-1307, USA.
- *****************************************************************/
-
 package chat.client.gui;
 
 import java.util.logging.Level;
@@ -58,12 +35,6 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-/**
- * This activity implement the main interface.
- * 
- * @author Michele Izzo - Telecomitalia
- */
 
 public class MainActivity extends Activity {
 	private Logger logger = Logger.getJADELogger(this.getClass().getName());
@@ -134,7 +105,7 @@ public class MainActivity extends Activity {
 			} else {
 				try {
 					SharedPreferences settings = getSharedPreferences(
-							"jadeChatPrefsFile", 0);
+							"jadePreferencesFile", 0);
 					String host = settings.getString("defaultHost", "");
 					String port = settings.getString("defaultPort", "");
 					infoTextView.setText(getString(R.string.msg_connecting_to)
--- android/app/src/main/java/chat/client/gui/SettingsActivity.java
+++ android/app/src/main/java/chat/client/gui/SettingsActivity.java
@@ -1,26 +1,3 @@
-/*****************************************************************
-JADE - Java Agent DEvelopment Framework is a framework to develop 
-multi-agent systems in compliance with the FIPA specifications.
-Copyright (C) 2000 CSELT S.p.A. 
-
-GNU Lesser General Public License
-
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation, 
-version 2.1 of the License. 
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the
-Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-Boston, MA  02111-1307, USA.
- *****************************************************************/
-
 package chat.client.gui;
 
 import jade.util.leap.Properties;
@@ -32,11 +9,6 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
-/**
- * This activity implement the settings interface.
- * 
- * @author Michele Izzo - Telecomitalia
- */
 
 public class SettingsActivity extends Activity {
 	Properties properties;
@@ -47,8 +19,7 @@ public class SettingsActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		SharedPreferences settings = getSharedPreferences("jadeChatPrefsFile",
-				0);
+		SharedPreferences settings = getSharedPreferences("jadePreferencesFile",0);
 
 		String host = settings.getString("defaultHost", "");
 		String port = settings.getString("defaultPort", "");
@@ -68,7 +39,7 @@ public class SettingsActivity extends Activity {
 	private OnClickListener buttonUseListener = new OnClickListener() {
 		public void onClick(View v) {
 			SharedPreferences settings = getSharedPreferences(
-					"jadeChatPrefsFile", 0);
+					"jadePreferencesFile", 0);
 			
 			// TODO: Verify that edited parameters was formally correct
 			SharedPreferences.Editor editor = settings.edit();
