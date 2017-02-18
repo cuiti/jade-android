79036d0b03a9846ae29b15a1b808f737c85a534c
Alfonso Cuitiño <alfonso_cuiti@hotmail.com>
2017-02-18 11:03:17 -0300

    limpieza menor del código

--- android/app/src/main/java/bolinocuitino/agentemovil/gui/ComunicacionActivity.java
+++ android/app/src/main/java/bolinocuitino/agentemovil/gui/ComunicacionActivity.java
@@ -245,7 +245,7 @@ public class ComunicacionActivity extends Activity {
 		int level = batteryIntent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
 		int scale = batteryIntent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
 
-		// Error checking that probably isn't needed but I added just in case.
+		// En caso de error...
 		if(level == -1 || scale == -1) {
 			return 50.0f;
 		}
